b3ab2b3255e616cffde8f77454031ff30ce40f95
Alexandre Tavares <xptavares@gmail.com>
2015-05-14 13:04:32 -0300

    teste i18n to pt

--- lib/pmf_nfpse_generator.rb
+++ lib/pmf_nfpse_generator.rb
@@ -44,7 +44,8 @@ class PmfNfpseGenerator
     attr_accessor :TipoSistema, :Emissor_Identificacao, :Emissor_AEDF, :Emissor_TipoAedf, :Emissor_Cidade, :Emissor_Estado, :Impostos
 
     I18n.load_path = Dir['./config/locales/*.yml']
-    I18n.locale = 'pt-BR'
+    I18n.locale = :pt
+    I18n.default_locale = :pt
   end
 
   # {"city"=>"Curitiba", "state"=>"PR", "city_ibge_code"=>"4106902", "source"=>"csv"}
